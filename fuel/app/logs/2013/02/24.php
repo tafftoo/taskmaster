7b2806c85145a49daaa06d235c752529f08416d4
David Jones <tafftoo@gmail.com>
2013-02-24 23:04:20 +0000

    Installed FuelPHP Framework 1.5

---
+++ fuel/app/logs/2013/02/24.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-02-24 22:55:31 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-02-24 23:00:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-02-24 23:00:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-02-24 23:00:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-02-24 23:01:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/app/views/welcome/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>FuelPHP Framework</title>
+	<?php echo Asset::css('bootstrap.css'); ?>
+</head>
+<body>
+	<div id="header">
+		<div class="row">
+			<div id="logo"></div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="hero-unit">
+			<h1>Welcome!</h1>
+		</div>
+		<footer>
+			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+		</footer>
+	</div>
+</body>
+</html>
